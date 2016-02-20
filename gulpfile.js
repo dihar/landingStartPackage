029ef738bd4b50b907feca2a6ec4d70d3b517728
dihar <diharin@gmail.com>
2016-02-20 10:27:59 +0300

    better structure

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,7 @@ var rename       = require("gulp-rename");
 var minify       = require("gulp-minify-css");
 var uglify       = require("gulp-uglify");
 var reload       = browserSync.reload;
+var sourcemaps   = require('gulp-sourcemaps');
 
 gulp.task('serve', function(){
 	browserSync.init({
@@ -25,16 +26,18 @@ gulp.task('js',function(){
 		.pipe(plumber({
 	    	errorHandler: notify.onError('JS error: <%= error.message %>')
 	    }))
+	    .pipe(sourcemaps.init())
 		.pipe(concat('main-min.js'))
 		.pipe(uglify())
+		.pipe(sourcemaps.write('./'))
 		.pipe(gulp.dest('js/'))
 		.pipe(browserSync.stream());
 });
 
 gulp.task('jq',function(){
-	return gulp.src(['bower_components/jquery/dist/jquery.min.js',
-			'bower_components/bootstrap/dist/js/bootstrap.min.js'])
+	return gulp.src(['bower_components/jquery/dist/jquery.js'])
 	.pipe(concat('jqBt.js'))
+	.pipe(uglify())
 	.pipe(gulp.dest('js/'));
 })
 
@@ -43,18 +46,21 @@ gulp.task('less', function(){
 		.pipe(plumber({
 	    	errorHandler: notify.onError('LESS error: <%= error.message %>')
 	    }))
+	    .pipe(sourcemaps.init())
 	    .pipe(less())
 	    .pipe(autoprefixer({
 	    	browsers: ['last 10 versions']
 	    }))
 	    .pipe(rename('style.css'))
+	    .pipe(gulp.dest('./less/'))
 	    .pipe(minify())
+	    .pipe(sourcemaps.write('less/'))
 	    .pipe(gulp.dest('./'))
 	    .pipe(browserSync.stream());
 })
 
 gulp.task('bootstrap', function(){
-	return gulp.src('bower_components/bootstrap/less/bootstrap.less')
+	return gulp.src('./less/bootstrap.less')
 		.pipe(less())
 	    .pipe(autoprefixer({
 	    	browsers: ['last 10 versions']
@@ -64,8 +70,11 @@ gulp.task('bootstrap', function(){
 	    .pipe(gulp.dest('./less/'));
 })
 
-gulp.watch(['js/main.js'], ['js'])
-gulp.watch(['less/*.less'], ['less'])
-gulp.watch(['./*.html'], reload)
+gulp.task('watch', function(){
+	gulp.watch(['js/main.js'], ['js'])
+	gulp.watch(['less/*.less'], ['less'])
+	gulp.watch(['./*.html'], reload)
+});
 
+gulp.task('service', ['bootstrap', 'jq']);
 gulp.task('default', ['serve']);
